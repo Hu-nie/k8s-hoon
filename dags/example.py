4cbb12a342c822a7e9f8febec928c60c5ab09d3c
hoon <hoon>
2024-03-18 02:16:50 +0900

    Test: api 데이터 볼수 있도록 수정 및 테스트

--- dags/example.py
+++ dags/example.py
@@ -4,9 +4,21 @@ from airflow.operators.dummy import DummyOperator
 from airflow.operators.python_operator import PythonVirtualenvOperator
 from airflow.operators.python_operator import PythonOperator
 
-def my_python_function():
-    print("Hello, Airflow!")
-    print("Current date:", datetime.now())
+import requests
+
+def fetch_and_print_api_data():
+    url = "http://api.hu-nie.com/data_collect"
+    
+    # API 요청
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        # 성공적으로 데이터를 받아왔을 때
+        data = response.json()
+        print(data)
+    else:
+        # 요청이 실패했을 때
+        print("API 요청에 실패했습니다. 상태 코드:", response.status_code)
 
 default_args = {
     'owner': 'airflow',
@@ -31,8 +43,8 @@ with DAG(
     start = DummyOperator(task_id = 'start')
     # 파이썬 함수 실행
     run_scraper = PythonOperator(
-        task_id='test',
-        python_callable=my_python_function,
+        task_id='fetch_and_print_api_data',
+        python_callable=fetch_and_print_api_data,
     )
 
     end = DummyOperator(task_id = 'end')
