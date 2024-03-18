1df4b54fc2c65eb5ba3749dc96273333f7776596
hoon <hoon>
2024-03-18 16:09:52 +0900

    Feat: DAG 파일 수정

--- dags/example.py
+++ dags/example.py
@@ -1,53 +1,41 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
-from airflow.operators.python_operator import PythonVirtualenvOperator
 from airflow.operators.python_operator import PythonOperator
-from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-
-import requests
-
-def fetch_data():
-    url = "http://api.hu-nie.com/data_collect"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        with open('/tmp/my_data.json', 'w') as f:
-            f.write(str(data))
-    else:
-        raise Exception("API 요청 실패")
-    
-    hook = S3Hook(aws_conn_id='test')
-    hook.load_file(filename='/tmp/my_data.json', key='my_data.json', bucket_name='hoon-s3-bucket', replace=True)
+from lib.get_data import fetch_and_upload_player_stats
 
 
 default_args = {
-    'owner': 'airflow',
+    'owner': 'hoon',
     'depends_on_past': False,
     'start_date': datetime(2023, 1, 1),
-    'email': ['your_email@example.com'],
+    'email': ['ehdgns8227@naver.com'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
 
-# DAG 정의
 with DAG(
-    'run_data_scraper',
+    'Basketball_Data_Scraper',
     default_args=default_args,
-    description='A simple DAG to run data_scraper.py',
-    schedule_interval=timedelta(days=1),
+    description='A simple DAG to run Basketball_Data_Scraper.py',
+    schedule_interval='0 0 * * *',  # 매일 자정에 실행
     catchup=False,
 ) as dag:
     
-    start = DummyOperator(task_id = 'start')
-    # 파이썬 함수 실행
+    start = DummyOperator(task_id='start')
+    
+    # 'yesterday_ds'는 Airflow가 제공하는 기본 변수 중 하나로,
+    # DAG이 실행되는 날짜(즉, 실행일 기준 '어제')를 'YYYY-MM-DD' 형식의 문자열로 제공합니다.
     run_scraper = PythonOperator(
-        task_id='fetch_data',
-        python_callable=fetch_data,
+        task_id='fetch_and_upload_data',
+        python_callable=fetch_and_upload_player_stats,
+        op_kwargs={
+            'date': '{{ yesterday_ds }}'
+        },
     )
 
-    end = DummyOperator(task_id = 'end')
+    end = DummyOperator(task_id='end')
 
-start >> run_scraper >> end 
+    start >> run_scraper >> end
---
+++ dags/lib/get_data.py
@@ -0,0 +1,27 @@
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+import json
+import requests
+
+def fetch_player_stats(date):
+    url = f"http://api.hu-nie.com/basketball/player-stats?date={date}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        file_path = f'/tmp/player_stats_{date}.json'
+        with open(file_path, 'w') as f:
+            json.dump(data, f)
+        print(f"데이터가 성공적으로 저장되었습니다: /tmp/player_stats_{date}.json")
+        return file_path
+    else:
+        raise Exception(f"API 요청 실패: 상태 코드 {response.status_code}")
+
+def upload_data_s3(file_path, date):
+    hook = S3Hook(aws_conn_id='your_aws_conn_id')
+    hook.load_file(filename=file_path, key= f'{date}_player-stats.json', bucket_name='hoon-s3-bucket', replace=True)
+
+def fetch_and_upload_player_stats(date):
+
+    file_path = fetch_player_stats(date)
+    upload_data_s3(file_path, date)
+    
+    print(f"{date}의 농구 선수 통계가 성공적으로 업로드되었습니다.")
